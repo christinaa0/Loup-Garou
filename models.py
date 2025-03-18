a7e9d9a9fb301cae0c194b53e74f17a9e553b95f
Christina Gapy <christina@MacBook-Pro-de-Christina.local>
2025-03-18 10:56:06 +0100

    mes changement

--- models.py
+++ models.py
@@ -3,13 +3,46 @@ import random
 
 class Game:
 
-    def __init__(self, nb_max_turn):
+    def __init__(self, nb_max_turn, width, height):
         self.__nb_max_turn = nb_max_turn # dunder => double underscore ?
         self.__current_turn = 0
+        self.__gameboard = GameBoard(height, width)
+        self.__actions = {} #c'est un dictionnaire
+
 
     @property
     def nb_max_turn(self):
         return self.__nb_max_turn
+    #action :(witdh,height) > indique de combien on se deplace de hauteur et de largeur (-1,0) , (1,0) , (0,1), (0,-1) {c'est un tuple}
+    def register_action(self, player, action):
+        self.__actions[player] = action
+
+    def process_action(self):
+        new_positions = {}  # Pour éviter des conflits de déplacement
+
+        for player, action in self.__actions.items():
+            current_x, current_y = self.find_player_position(player)  # Trouver la position actuelle
+            new_x = current_x + action[0]
+            new_y = current_y + action[1]
+
+            # Vérifier si la nouvelle position est valide et libre
+            if 0 <= new_x < self.__gameboard._GameBoard__height and 0 <= new_y < self.__gameboard._GameBoard__width:
+                if self.__gameboard._GameBoard__content[new_x][new_y] == 'x':  # Vérifier si la case est vide
+                    new_positions[player] = (new_x, new_y)
+
+        # Appliquer les déplacements après avoir tout vérifié
+        for player, (new_x, new_y) in new_positions.items():
+            current_x, current_y = self.find_player_position(player)
+            self.__gameboard._GameBoard__content[current_x][current_y] = 'x'  # Effacer l'ancienne position
+            self.__gameboard._GameBoard__content[new_x][new_y] = player  # Mettre à jour la nouvelle position
+
+    def find_player_position(self, player):
+        """ Trouve la position actuelle d'un joueur sur le plateau """
+        for x in range(self.__gameboard._GameBoard__height):
+            for y in range(self.__gameboard._GameBoard__width):
+                if self.__gameboard._GameBoard__content[x][y] == player:
+                    return x, y
+        return None, None  # Si jamais on ne trouve pas le joueur (erreur)
 
 
 class Player(ABC):
@@ -37,12 +70,15 @@ class Wolf(Player):
 
     def __init__(self, pseudo):
         super().__init__(pseudo, 2)
-
+    def __str__(self):
+        return 'W'
 
 
 class Villager(Player):
     def __init__(self, pseudo):
         super().__init__(pseudo, 1)
+    def __str__(self):
+        return 'O'
 
 
 class GameBoard:
@@ -52,10 +88,15 @@ class GameBoard:
         self.__width = width
         self.__content = [['x'] * width for _ in range(height)]
 
-    def subscribe_player(self, player : Player):
-        player_height = random.randrange(0, self.__height)
-        player_width = random.randrange(0, self.__width)
-        self.__content[player_height][player_width] = player
+    def subscribe_player(self, player: Player):
+        while True:
+            player_height = random.randrange(0, self.__height)
+            player_width = random.randrange(0, self.__width)
+
+            # Vérifier que la case est vide
+            if self.__content[player_height][player_width] == 'x':
+                self.__content[player_height][player_width] = player
+                break  # Sortir de la boucle une fois que le joueur est bien placé
 
     def __repr__(self):
         result = ''
